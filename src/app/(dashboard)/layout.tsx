d3b0e4a2a6de1973f116615a75a59520138848e7
Darby Wright <darbyw@uoregon.edu>
2025-03-28 15:34:11 -0700

    Finished Admin Page

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -5,15 +5,15 @@ import Link from "next/link";
 
 export default function DashboardLayout({
   children,
-  }: Readonly<{
-    children: React.ReactNode;
+}: Readonly<{
+  children: React.ReactNode;
   }>) {
     return <div className="h-screen flex">
       {/* LEFT */}
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link href="/" className="flex items-center justify-center lg:justify-start" >
           <Image src="/logo.png" alt="logo" width={32} height={32}/>
-          <span className="hidden lg:block">SchooLama</span>
+          <span className="hidden lg:block font-bold">SchooLama</span>
         </Link>
         <Menu/>
       </div>
