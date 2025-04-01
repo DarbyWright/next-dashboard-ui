0476bd0a1926810058c5db5150e7e5356209996e
Darby Wright <wright.darby@yahoo.com>
2025-04-01 12:46:07 -0700

    Partial Student Page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
           <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full">1</div>
         </div>
         <div className="flex flex-col">
-          <span className="text-xs leading-3 font-medium">John Doe</span>
+          <span className="text-xs leading-3 font-medium">Darby Wright</span>
           <span className="text-[10px] text-gray-500 text-right">Admin</span>
         </div>
         <div>
